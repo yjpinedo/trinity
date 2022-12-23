b054b6bc3def6c8e31ba7996f2c5233b54d1da58
Yeimer Pinedo <yjpinedo@unicesar.edu.co>
2022-12-22 21:48:55 -0500

    change styles alert verify-email.blade.php

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,10 +1,9 @@
 <x-layouts.guest body="register-page" box="register-box" title="{{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}">
 
     @if (session('status') == 'verification-link-sent')
-        <div class="alert alert-info alert-dismissible">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            <h5><i class="icon fas fa-info"></i> Alert!</h5>
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+        <div class="callout callout-info">
+            <h5>{{ __('Information') }}</h5>
+            <p>{{ __('A new verification link has been sent to the email address you provided during registration.') }}</p>
         </div>
     @endif
 
